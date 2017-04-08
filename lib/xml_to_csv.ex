53462f415bd2022edb974f8d141a390df4dff247
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-04-08 15:15:52 +0530

    init

---
+++ lib/xml_to_csv.ex
@@ -0,0 +1,18 @@
+defmodule XmlToCsv do
+  @moduledoc """
+  Documentation for XmlToCsv.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> XmlToCsv.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xml_to_csv_test.exs
@@ -0,0 +1,8 @@
+defmodule XmlToCsvTest do
+  use ExUnit.Case
+  doctest XmlToCsv
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
