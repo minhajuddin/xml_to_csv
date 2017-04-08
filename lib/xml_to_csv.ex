8b00e43ae57d1a0779af956ba5a81380a50de011
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-04-08 15:27:43 +0530

    save

--- lib/xml_to_csv.ex
+++ lib/xml_to_csv.ex
@@ -1,18 +1,32 @@
 defmodule XmlToCsv do
-  @moduledoc """
-  Documentation for XmlToCsv.
-  """
+  import Logger, only: [info: 1, error: 1]
 
-  @doc """
-  Hello world.
+  def main([]) do
+    error """
+    USAGE:
+      xml_to_csv schema_file_path file1.xml file2.xml ...
+    """
+  end
+  def main([schema_file | files]) do
+    files
+    |> Enum.with_index(1)
+    |> Task.async_stream(fn {file, index} ->
+      process_file(file, index, schema)
+    end, max_concurrency: System.schedulers_online, timeout: :infinity)
+  end
 
-  ## Examples
+  defp process_file(file, index, schema) do
+    import SweetXml
+    info "#{index} processing #{file}"
+    file
+    |> File.read!
+    |> parse
+    |> transform_to_csv_row(schema)
+    |> CSV.encode
+    |> Enum.into(File.stream!("#{file}.csv"))
+    info "#{index} finished processing #{file}"
+  end
 
-      iex> XmlToCsv.hello
-      :world
-
-  """
-  def hello do
-    :world
+  defp transform_to_csv_row(xml, schema) do
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule XmlToCsv.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: XmlToCsv],
      deps: deps()]
   end
 
